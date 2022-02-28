1c262fdd638a4a1049c92bc5416ddf7664c27d41
Behruz-01 <bekhruzgaybullaev@gmail.com>
2022-03-01 02:16:05 +0500

    initialy commit

---
+++ js/main.js
@@ -0,0 +1,74 @@
+var elForm = document.querySelector(".form")
+var elInput = document.querySelector(".form__input")
+var elButtton = document.querySelector(".form__btn")
+var elMan = document.querySelector(".head__man")
+var elValo = document.querySelector(".head__velo")
+var elCar = document.querySelector(".head__car")
+var elAirplane = document.querySelector(".head__airplane")
+
+
+elForm.addEventListener("submit" , function(event){
+    event.preventDefault();
+
+    var elValue = elInput.value;
+    var Person = 3.6;
+    var Velosiped = 20.1;
+    var Mashina = 70;
+    var Plane = 800;
+    var elResult = document.querySelector(".result")
+
+
+    if (isNaN(elValue)){
+        elResult.textContent = "son kiritish kerak";
+
+
+        elResult.classList.add("result-error");
+        elResult.classList.remove("result-success");
+
+
+        return
+    }
+
+
+    if (elValue <=0 ){
+        elResult.textContent = "0 dan katta son kiriting";
+
+
+        elResult.classList.add("result-error");
+        elResult.classList.remove("result-success");
+
+
+        return
+    }
+
+    else{
+        elResult.textContent = "ma'lumot to'g'ri kiritildi";
+        elResult.classList.add("result-success");
+        elResult.classList.remove("result-error");
+    }
+
+
+    function people() {
+        return elValue/Person;
+    }
+    function velo() {
+        return elValue/Velosiped;
+    }
+
+    function car() {
+        return elValue/Mashina;
+    }
+
+    function airplane() {
+        return elValue/Plane;
+    }
+
+
+
+
+
+    elMan.textContent = Math.round(people());
+    elValo.textContent = Math.round(velo());
+    elCar.textContent = Math.round(car());
+    elAirplane.textContent = Math.round(airplane());
+})
